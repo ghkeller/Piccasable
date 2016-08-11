009e4692f1c9ee6e701a7557405a818fc31af851
ghkeller <ghkeller@ucsc.edu>
2016-08-10 23:46:53 -0700

    Added basic upload funct. as standalone application

---
+++ upload.cc
@@ -0,0 +1,70 @@
+#include <functional>
+#include <Wt/WApplication>
+#include <Wt/WBreak>
+#include <Wt/WContainerWidget>
+#include <Wt/WFileUpload>
+#include <Wt/WProgressBar>
+#include <Wt/WPushButton>
+#include <Wt/WText>
+
+class UploadApplication : public Wt::WApplication
+{
+public:
+    UploadApplication(const Wt::WEnvironment& env);
+
+private:
+	Wt::WContainerWidget *container;
+	Wt::WFileUpload *fu;
+	Wt::WPushButton *uploadButton;
+	Wt::WText *out;
+};
+
+UploadApplication::UploadApplication(const Wt::WEnvironment& env)
+    : Wt::WApplication(env)
+{
+    container = new Wt::WContainerWidget();
+    root()->addWidget(container);
+	fu = new Wt::WFileUpload(container);
+	fu->setFileTextSize(50); // Set the maximum file size to 50 kB.
+	fu->setProgressBar(new Wt::WProgressBar());
+	fu->setMargin(10, Wt::Right);
+
+	// Provide a button to start uploading.
+	uploadButton = new Wt::WPushButton("Send", container);
+	uploadButton->setMargin(10, Wt::Left | Wt::Right);
+
+	out = new Wt::WText(container);
+
+	// Upload when the button is clicked.
+	uploadButton->clicked().connect(std::bind([=] () {
+	    fu->upload();
+	    uploadButton->disable();
+	}));
+
+	// Upload automatically when the user entered a file.
+	fu->changed().connect(std::bind([=] () {
+	    fu->upload();
+	    uploadButton->disable();
+	    out->setText("File upload is changed.");
+	}));
+
+	// React to a succesfull upload.
+	fu->uploaded().connect(std::bind([=] () {
+	    out->setText("File upload is finished.");
+	}));
+
+	// React to a file upload problem.
+	fu->fileTooLarge().connect(std::bind([=] () {
+	    out->setText("File is too large.");
+	}));
+}
+
+Wt::WApplication *createApplication(const Wt::WEnvironment& env)
+{
+    return new UploadApplication(env);
+}
+
+int main(int argc, char **argv)
+{
+    return Wt::WRun(argc, argv, &createApplication);
+}
